1e21d82f5dabe0933ca4b98540b3d87ca385abbf
Neel Smith <dnsmith.neel@gmail.com>
2014-10-26 13:27:57 -0400

    Add script to reboot tomcat.

---
+++ bin/reboot-tc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env /bin/bash
+#
+# bash script for rebooting tomcat within the CITE virtual machine,
+# citevm
+#
+
+echo "Buidling war..."
+/vagrant/bin/build-war.sh /vagrant/repositories/mss/confs/vm-servletconf.gradle -l /vagrant/repositories/mss/confs/vm-servletlinks.gradle -o /vagrant/repositories/mss/servlet
+
+
+echo "Loading new war into tomcat ..."
+/vagrant/bin/run-war.sh
